8ff316b0b85050ea12482f0b2194cb1f10508fce
Aslynx <anthony.pierr23@gmail.com>
2018-03-01 15:39:08 +0100

    Improvement of User form controls

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -21,30 +21,50 @@ $(document).ready(function() {
             error++;
             username.parent().addClass('has-error');
             username.parent().append('<span class="text-danger form-error">Champ vide.</span>');
+        } else if(username.val().match(/^[aA-zZ0-9]+([-._][a-z0-9]+)*$/i) == null) {
+            error++;
+            username.parent().addClass('has-error');
+            username.parent().append('<span class="text-danger form-error">Pseudo invalide.</span>');
         }
 
         if(street.val() == "") {
             error++;
             street.parent().addClass('has-error');
             street.parent().append('<span class="text-danger form-error">Champ vide.</span>');
+        } else if(street.val().match(/^[aA-zZ0-9]+([\s-][aA-zZ0-9]+)*$/i) == null) {
+            error++;
+            street.parent().addClass('has-error');
+            street.parent().append('<span class="text-danger form-error">Rue invalide.</span>');
         }
 
         if(city.val() == "") {
             error++;
             city.parent().addClass('has-error');
             city.parent().append('<span class="text-danger form-error">Champ vide.</span>');
+        } else if(city.val().match(/^[aA-zZ]+([-][aA-zZ]+)*$/i) == null) {
+            error++;
+            city.parent().addClass('has-error');
+            city.parent().append('<span class="text-danger form-error">Ville invalide.</span>');
         }
 
         if(state.val() == "") {
             error++;
             state.parent().addClass('has-error');
             state.parent().append('<span class="text-danger form-error">Champ vide.</span>');
+        } else if(state.val().match(/^[aA-zZ]+([-][aA-zZ]+)*$/i) == null) {
+            error++;
+            state.parent().addClass('has-error');
+            state.parent().append('<span class="text-danger form-error">Etat invalide.</span>');
         }
 
         if(country.val() == "") {
             error++;
             country.parent().addClass('has-error');
             country.parent().append('<span class="text-danger form-error">Champ vide.</span>');
+        } else if(country.val().match(/^[aA-zZ]+([-][aA-zZ]+)*$/i) == null) {
+            error++;
+            country.parent().addClass('has-error');
+            country.parent().append('<span class="text-danger form-error">Pays invalide.</span>');
         }
 
         if(error == 0) {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,22 +58,70 @@ class UsersController < ApplicationController
   
       user_params_form
   
-      flash.notice = "User '#{@user.username}' Updated!"
     end
   
     private
       def user_params_form
-        @user.username = params[:user][:username]
-        @user.street = params[:user][:street]
-        @user.city = params[:user][:city]
-        @user.state = params[:user][:state]
-        @user.country = params[:user][:country]
-        @user.avatar = params[:user][:avatar]
-  
-        @user.save
-  
-        redirect_to user_path(@user)
+
+        if(check_user_params)
+          @user.username = params[:user][:username]
+          @user.street = params[:user][:street]
+          @user.city = params[:user][:city]
+          @user.state = params[:user][:state]
+          @user.country = params[:user][:country]
+          @user.avatar = params[:user][:avatar]
+    
+          @user.save
+    
+          redirect_to user_path(@user)
+
+          flash.notice = "User '#{@user.username}' Updated!"
+        else
+
+          redirect_to edit_user_path(@user)
+
+          flash.notice = "The edit had a problem. Please try again"
+        end
+        
       end
-  
+
+      def check_user_params
+        if !check_username(params[:user][:username])
+          puts "Username"
+          return false
+        elseif !check_street(params[:user][:street])
+          puts "Street"
+          return false
+        elseif !check_address(params[:user][:city])
+          puts "City"
+          return false
+        elseif !check_address(params[:user][:state])
+          puts "State"
+          return false
+        elseif !check_address(params[:user][:country])
+          puts "Country"
+          return false
+        else
+          return true
+        end
+      end
+
+      def check_object(object)
+        return !object.empty?
+      end
+
+      def check_username(username)
+        return check_object(username) && !username.match(/^[aA-zZ0-9]+([-._][a-z0-9]+)*$/i).nil?
+      end
+
+      def check_street(street)
+        return check_object(street) && !street.match(/^[aA-zZ0-9]+([\s-][aA-zZ0-9]+)*$/i).nil?
+      end
+
+      def check_address(address)
+        return check_object(address) && !address.match(/^[aA-zZ]+([-][aA-zZ]+)*$/i).nil?
+      
+      end
+
   end
   
