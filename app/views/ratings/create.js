9951f4fb160cc9a37ccc48b437284b00fa1e9d5e
Aslynx <anthony.pierr23@gmail.com>
2018-03-06 14:48:40 +0100

    Fix average rating disappearance in js

--- app/views/ratings/create.js
+++ app/views/ratings/create.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-    console.log("Simulated")
+    console.log("Voted")
 
-    $('#average-result').parent().remove();//"<%= j render(partial: 'ratings/form') %>");
+    //$('#average-result').parent().remove();//"<%= j render(partial: 'ratings/form') %>");
     $('#average-result').parent().html("<%= j render(partial: 'games/rating')%>");
+    
+    //$(this).parent().html("<%= j render(partial: 'games/rating')%>");
     console.log($(this).parent())
     //location.reload();
 })
